2bd5800143409a6e1ee4eb7c93d4b3cb35195c84
cony89 <alessandro.bigiotti@libero.it>
2016-09-21 13:35:27 +0200

    Update Program.c

--- Program.c
+++ Program.c
@@ -1,6 +1,5 @@
 /*Created by Alessandro Bigiotti*/
 
-
 // Inlcude C library
 #include <stdio.h>
 #include <stdlib.h>
@@ -21,21 +20,21 @@
 #include "CL/cl_ext.h"
 #include "CL/opencl.h"
 
-// Include the file with the data structures
+// Include the file containing the data structures
 #include "./dataStruct.c"
 
-// Include a file with the OpenCL Inizialization
+// Include a file containing the OpenCL Inizialization
 #include "./setOpenCLAmbient.c"
 
-// Include a file with the Kernel Inizialization
+// Include a file containing the Kernel Inizialization
 #include "./initKernel.c"
 
-// Include a file with the
+// Include a file containing the Kernel Configuration
 #include "./setKernel.c"
 
 #define MAX_SOURCE_SIZE (0x100000)
 
-// Define the prototype of the auxiliary function
+// Define the prototype of the auxiliary functions
 int initilizeOpenCL();
 int CreateKernel();
 int SetKernel(int W, int H);
@@ -63,21 +62,21 @@ int main()
 	ILubyte* raw_image = ilGetData();
 
 	// Retrieve the WIDTH and the HEIGHT of the image
-  ILuint W = ilGetInteger(IL_IMAGE_WIDTH);
+  	ILuint W = ilGetInteger(IL_IMAGE_WIDTH);
 	ILuint H = ilGetInteger(IL_IMAGE_HEIGHT);
-  printf ("Dimensioni Immagine: %d, %d \n", W, H);
+  	printf ("Dimensioni Immagine: %d, %d \n", W, H);
 
 	// Buffer to copy the pixels
 	data_raw = (ILubyte*)malloc(sizeof(ILubyte)*H*W * 3);
 
-	// Calculate the image pixels Dimensions
+	// Calculate the image Dimensions in pixels
 	unsigned int S = W * H * 3;
 
 	// Copy the pixels of the image into data_raw
 	ilCopyPixels(0, 0, 0, W, H, 1, IL_RGB, IL_UNSIGNED_BYTE, data_raw);
 
 	// Some Vector for ILubyte* output image;
-  ILubyte* out_result = (ILubyte*)malloc(sizeof(ILubyte)*S);
+  	ILubyte* out_result = (ILubyte*)malloc(sizeof(ILubyte)*S);
 	ILubyte* out_result2 = (ILubyte*)malloc(sizeof(ILubyte)*S);
 	ILubyte* out_result3 = (ILubyte*)malloc(sizeof(ILubyte)*S);
 	ILubyte* out_result4 = (ILubyte*)malloc(sizeof(ILubyte)*S);
@@ -89,8 +88,7 @@ int main()
 	float* output3 = (float*)malloc(sizeof(float)*S);
 	float* output4 = (float*)malloc(sizeof(float)*S);
 
-  // Normalize the pixels to 255 (float values)
-	//per occupare meno memoria
+  	// Normalize the pixels to 255 (float values) to reduce GPU Memory 
 	for (int i=0; i < S; i+=3)	{
 		data[i + 0] = (float)(data_raw[i + 0] / 255.0);
 		data[i + 1] = (float)(data_raw[i + 1] / 255.0);
@@ -98,11 +96,11 @@ int main()
 	}
 
 
-  InitializeOpenCL();
+  	InitializeOpenCL();
 
-  CreateKernel();
+  	CreateKernel();
 
-  // Copy all necessary data into GPU
+  	// Copy all necessary data into GPU
 	dev_image = clCreateBuffer(context, CL_MEM_READ_WRITE | CL_MEM_COPY_HOST_PTR,
 		sizeof(float)*S, data, &err);
 	out_put = clCreateBuffer(context, CL_MEM_READ_WRITE, sizeof(float)*S, NULL, &err);
@@ -126,129 +124,129 @@ int main()
 	sharp = clCreateBuffer(context, CL_MEM_READ_WRITE | CL_MEM_COPY_HOST_PTR,
 		sizeof(float) * 9, sharpening, &err);
 	if (err < 0){
-		printf("Impossibile copire i dati su GPU!\n");
+		printf("Impossible copy the data on GPU!\n");
 		return EXIT_FAILURE;
 	}
 
 	///////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-  //Dimensione WorkGroup per operazioni puntuali
-  size_t globalWorkSize3D[3] = {W,H,3};
-  //defauls localWorkSize3D { 1, 1, 1 }
+  	//Dimensione WorkGroup per operazioni puntuali
+  	size_t globalWorkSize3D[3] = {W,H,3};
+  	//defauls localWorkSize3D { 1, 1, 1 }
 
-  //Dimensione WorkGroup per operazioni locali
-  size_t globalWorkSize2D[2] = { W * 3, H };
-  //default localWorkSize2D { 1, 1 }
+  	//Dimensione WorkGroup per operazioni locali
+  	size_t globalWorkSize2D[2] = { W * 3, H };
+  	//default localWorkSize2D { 1, 1 }
 
-  // Set the kernel parameters
-  SetKernel(W,H);
+  	// Set the kernel parameters
+  	SetKernel(W,H);
 
-  // Select the Kernel to Run
-  printf(" Select the filter to applicate:\n");
-  printf("    1: Negative filter \n");
-  printf("    2: Binarization filter \n");
-  printf("    3: Gauss (5x5) \n");
-  printf("    4: Smooth (5x5) \n");
-  printf("    5: Prewitt North (3x3) \n");
-  printf("    6: Prewitt East (3x3) \n");
-  printf("    7: Sharp (3x3) \n");
-  printf("    8: Edge Detection \n");
-  scanf("%d", &filter_selected);
+  	// Select the Kernel to Run
+  	printf(" Select the filter to applicate:\n");
+  	printf("    1: Negative filter \n");
+  	printf("    2: Binarization filter \n");
+  	printf("    3: Gauss (5x5) \n");
+  	printf("    4: Smooth (5x5) \n");
+  	printf("    5: Prewitt North (3x3) \n");
+  	printf("    6: Prewitt East (3x3) \n");
+  	printf("    7: Sharp (3x3) \n");
+  	printf("    8: Edge Detection \n");
+  	scanf("%d", &filter_selected);
 
-  if(filter_selected == 1){
-    printf("Start image changes...\n");
-    err = clEnqueueNDRangeKernel(commands, kernel_negativo, 3, NULL, globalWorkSize3D, NULL,
-      0, NULL,NULL);
-    err |= clEnqueueReadBuffer(commands, out_put, CL_TRUE, 0, sizeof(float)*S, output, 0, NULL, NULL);
-    if (err != CL_SUCCESS)  {
-      printf("Errore, Impossibile leggere array di output! %d\n", err);
-      exit(EXIT_FAILURE);
-    }
-  }
-  else if(filter_selected == 2){
-    printf("Start image changes...\n");
-    err = clEnqueueNDRangeKernel(commands, binarizza, 3, NULL, globalWorkSize3D, NULL,
-      0, NULL,NULL);
-    err |= clEnqueueReadBuffer(commands, out_put, CL_TRUE, 0, sizeof(float)*S, output, 0, NULL, NULL);
-    if (err != CL_SUCCESS)  {
-      printf("Errore, Impossibile leggere array di output! %d\n", err);
-      exit(EXIT_FAILURE);
-    }
-  }
-  else if(filter_selected == 3){
-    printf("Start image changes...\n");
-    err = clEnqueueNDRangeKernel(commands, kernel_mask_factor, 2, NULL, globalWorkSize2D, NULL,
-      0, NULL,NULL);
-    err |= clEnqueueReadBuffer(commands, out_put, CL_TRUE, 0, sizeof(float)*S, output, 0, NULL, NULL);
-    if (err != CL_SUCCESS)  {
-      printf("Errore, Impossibile leggere array di output! %d\n", err);
-      exit(EXIT_FAILURE);
-    }
-  }
-  else if(filter_selected == 4){
-    printf("Start image changes...\n");
-    err = clEnqueueNDRangeKernel(commands, kernel_mask_factor2, 2, NULL, globalWorkSize2D, NULL,
-      0, NULL,NULL);
-    err |= clEnqueueReadBuffer(commands, out_put, CL_TRUE, 0, sizeof(float)*S, output, 0, NULL, NULL);
-    if (err != CL_SUCCESS)  {
-      printf("Errore, Impossibile leggere array di output! %d\n", err);
-      exit(EXIT_FAILURE);
-    }
-  }
-  else if(filter_selected == 5){
-    printf("Start image changes...\n");
-    err = clEnqueueNDRangeKernel(commands, kernel_mask, 2, NULL, globalWorkSize2D, NULL,
-      0, NULL,NULL);
-    err |= clEnqueueReadBuffer(commands, out_put, CL_TRUE, 0, sizeof(float)*S, output, 0, NULL, NULL);
-    if (err != CL_SUCCESS)  {
-      printf("Errore, Impossibile leggere array di output! %d\n", err);
-      exit(EXIT_FAILURE);
-    }
-  }
-  else if(filter_selected == 6){
-    printf("Start image changes...\n");
-    err = clEnqueueNDRangeKernel(commands, kernel_mask2, 2, NULL, globalWorkSize2D, NULL,
-      0, NULL,NULL);
-    err |= clEnqueueReadBuffer(commands, out_put, CL_TRUE, 0, sizeof(float)*S, output, 0, NULL, NULL);
-    if (err != CL_SUCCESS)  {
-      printf("Errore, Impossibile leggere array di output! %d\n", err);
-      exit(EXIT_FAILURE);
-    }
-  }
-  else if(filter_selected == 7){
-    printf("Start image changes...\n");
-    err = clEnqueueNDRangeKernel(commands, kernel_mask3, 2, NULL, globalWorkSize2D, NULL,
-      0, NULL,NULL);
-    err |= clEnqueueReadBuffer(commands, out_put, CL_TRUE, 0, sizeof(float)*S, output, 0, NULL, NULL);
-    if (err != CL_SUCCESS)  {
-      printf("Errore, Impossibile leggere array di output! %d\n", err);
-      exit(EXIT_FAILURE);
-    }
-  }
-  else if(filter_selected == 8){
-    printf("Start image changes...\n");
-    err = clEnqueueNDRangeKernel(commands, edge_detection, 2, NULL, globalWorkSize2D, NULL,
-      0, NULL,NULL);
-    err |= clEnqueueReadBuffer(commands, out_put, CL_TRUE, 0, sizeof(float)*S, output, 0, NULL, NULL);
-    if (err != CL_SUCCESS)  {
-      printf("Errore, Impossibile leggere array di output! %d\n", err);
-      exit(EXIT_FAILURE);
-    }
-  }
-  clFinish(commands);
+  	if(filter_selected == 1){
+    		printf("Start image changes...\n");
+    		err = clEnqueueNDRangeKernel(commands, kernel_negativo, 3, NULL, globalWorkSize3D, NULL,
+      			0, NULL,NULL);
+    		err |= clEnqueueReadBuffer(commands, out_put, CL_TRUE, 0, sizeof(float)*S, output, 0, NULL, NULL);
+    		if (err != CL_SUCCESS)  {
+      			printf("Errore, Impossibile leggere array di output! %d\n", err);
+      			exit(EXIT_FAILURE);
+    		}
+  	}
+  	else if(filter_selected == 2){
+    		printf("Start image changes...\n");
+    		err = clEnqueueNDRangeKernel(commands, binarizza, 3, NULL, globalWorkSize3D, NULL,
+      			0, NULL,NULL);
+    		err |= clEnqueueReadBuffer(commands, out_put, CL_TRUE, 0, sizeof(float)*S, output, 0, NULL, NULL);
+    		if (err != CL_SUCCESS)  {
+      			printf("Errore, Impossibile leggere array di output! %d\n", err);
+      			exit(EXIT_FAILURE);
+    		}
+  	}
+  	else if(filter_selected == 3){
+    		printf("Start image changes...\n");
+    		err = clEnqueueNDRangeKernel(commands, kernel_mask_factor, 2, NULL, globalWorkSize2D, NULL,
+      			0, NULL,NULL);
+    		err |= clEnqueueReadBuffer(commands, out_put, CL_TRUE, 0, sizeof(float)*S, output, 0, NULL, NULL);
+    		if (err != CL_SUCCESS)  {
+      			printf("Errore, Impossibile leggere array di output! %d\n", err);
+      			exit(EXIT_FAILURE);
+    		}
+  	}
+  	else if(filter_selected == 4){
+    		printf("Start image changes...\n");
+    		err = clEnqueueNDRangeKernel(commands, kernel_mask_factor2, 2, NULL, globalWorkSize2D, NULL,
+      			0, NULL,NULL);
+    		err |= clEnqueueReadBuffer(commands, out_put, CL_TRUE, 0, sizeof(float)*S, output, 0, NULL, NULL);
+    		if (err != CL_SUCCESS)  {
+      			printf("Errore, Impossibile leggere array di output! %d\n", err);
+      			exit(EXIT_FAILURE);
+    		}
+  	}
+  	else if(filter_selected == 5){
+    		printf("Start image changes...\n");
+    		err = clEnqueueNDRangeKernel(commands, kernel_mask, 2, NULL, globalWorkSize2D, NULL,
+      			0, NULL,NULL);
+    		err |= clEnqueueReadBuffer(commands, out_put, CL_TRUE, 0, sizeof(float)*S, output, 0, NULL, NULL);
+    		if (err != CL_SUCCESS)  {
+      			printf("Errore, Impossibile leggere array di output! %d\n", err);
+      			exit(EXIT_FAILURE);
+    		}
+  	}
+  	else if(filter_selected == 6){
+    		printf("Start image changes...\n");
+    		err = clEnqueueNDRangeKernel(commands, kernel_mask2, 2, NULL, globalWorkSize2D, NULL,
+      			0, NULL,NULL);
+    		err |= clEnqueueReadBuffer(commands, out_put, CL_TRUE, 0, sizeof(float)*S, output, 0, NULL, NULL);
+    		if (err != CL_SUCCESS)  {
+      			printf("Errore, Impossibile leggere array di output! %d\n", err);
+      			exit(EXIT_FAILURE);
+    		}
+  	}
+  	else if(filter_selected == 7){
+    		printf("Start image changes...\n");
+    		err = clEnqueueNDRangeKernel(commands, kernel_mask3, 2, NULL, globalWorkSize2D, NULL,
+      			0, NULL,NULL);
+    		err |= clEnqueueReadBuffer(commands, out_put, CL_TRUE, 0, sizeof(float)*S, output, 0, NULL, NULL);
+    		if (err != CL_SUCCESS)  {
+      			printf("Errore, Impossibile leggere array di output! %d\n", err);
+      			exit(EXIT_FAILURE);
+    		}
+  	}
+  	else if(filter_selected == 8){
+    		printf("Start image changes...\n");
+    		err = clEnqueueNDRangeKernel(commands, edge_detection, 2, NULL, globalWorkSize2D, NULL,
+      			0, NULL,NULL);
+    		err |= clEnqueueReadBuffer(commands, out_put, CL_TRUE, 0, sizeof(float)*S, output, 0, NULL, NULL);
+    		if (err != CL_SUCCESS)  {
+      			printf("Errore, Impossibile leggere array di output! %d\n", err);
+      			exit(EXIT_FAILURE);
+    		}
+  	}
+  	clFinish(commands);
 
-  //Conversione del risultato (float) in byte
+  	//Conversione del risultato (float) in byte
 	for (int i = 0; i < S; i += 3){
 		out_result[i + 0] = (ILubyte)(output[i + 0] * 255.0f);
 		out_result[i + 1] = (ILubyte)(output[i + 1] * 255.0f);
 		out_result[i + 2] = (ILubyte)(output[i + 2] * 255.0f);
-  }
-  //Copio il risultato in un'immagine specificata da result#
-  ilTexImage(W, H, 1, 3, IL_RGB, IL_UNSIGNED_BYTE, out_result);
-  ilEnable(IL_FILE_OVERWRITE);
-  ilSave(IL_BMP, result);
+  	}
+  	//Copio il risultato in un'immagine specificata da result#
+  	ilTexImage(W, H, 1, 3, IL_RGB, IL_UNSIGNED_BYTE, out_result);
+  	ilEnable(IL_FILE_OVERWRITE);
+  	ilSave(IL_BMP, result);
 
-  printf("Changes Completed! \n");
-  //libero le risorse
+  	printf("Changes Completed! \n");
+  	//libero le risorse
 	clReleaseMemObject(dev_image);
 	clReleaseMemObject(out_put);
 	clReleaseMemObject(out_put2);
